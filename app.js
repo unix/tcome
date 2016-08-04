fe83295dc512e43b78c29d1a045e55280c4757c5
WittBulter <nanazuimeng123@gmail.com>
2016-08-04 09:36:22 +0800

    Create

--- app.js
+++ app.js
@@ -1,59 +1,48 @@
 /**
- * app.js
- *
- * Use `app.js` to run your app without `sails lift`.
- * To start the server, run: `node app.js`.
- *
- * This is handy in situations where the sails CLI is not relevant or useful.
- *
- * For example:
- *   => `node app.js`
- *   => `forever start app.js`
- *   => `node debug app.js`
- *   => `modulus deploy`
- *   => `heroku scale`
- *
- *
- * The same command-line arguments are supported, e.g.:
- * `node app.js --silent --port=80 --prod`
+ * Created by WittBulter on 16/8/4.
  */
 
-// Ensure we're in the project directory, so relative paths work as expected
-// no matter where we actually lift from.
-process.chdir(__dirname);
+const express = require('express')
+const bodyParser = require ('body-parser')
+const http = require('http')
+const debug = require ('debug') ('itsmycar:server')
+const logger = require ('morgan')
+const routes = require ('./routes')
+const app = express()
 
-// Ensure a "sails" can be located:
-(function() {
-  var sails;
-  try {
-    sails = require('sails');
-  } catch (e) {
-    console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
-    console.error('To do that, run `npm install sails`');
-    console.error('');
-    console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
-    console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
-    console.error('but if it doesn\'t, the app will run with the global sails instead!');
-    return;
-  }
+const port = (process.env.PORT || 3000)
+app.set ('port', port)
+const server = http.createServer (app)
+server.listen (port, _ => console.log(
+	`--------    Express Message    --------
+this Service is started, PORT => ${port}
+**`))
 
-  // Try to get `rc` dependency
-  var rc;
-  try {
-    rc = require('rc');
-  } catch (e0) {
-    try {
-      rc = require('sails/node_modules/rc');
-    } catch (e1) {
-      console.error('Could not find dependency: `rc`.');
-      console.error('Your `.sailsrc` file(s) will be ignored.');
-      console.error('To resolve this, run:');
-      console.error('npm install rc --save');
-      rc = function () { return {}; };
-    }
-  }
+server.on('error', error =>{
+	if (error.syscall !== 'listen') throw error
+	const bind = typeof port === 'string'? 'Pipe ' + port: 'Port ' + port
 
-
-  // Start server
-  sails.lift(rc('sails'));
-})();
+	switch (error.code) {
+		case 'EACCES':
+			console.error (bind + ' requires elevated privileges')
+			process.exit (1)
+			break;
+		case 'EADDRINUSE':
+			console.error (bind + ' is already in use')
+			process.exit (1)
+			break;
+		default:
+			throw error;
+	}
+})
+server.on ('listening', _ =>{
+	const addr = server.address ()
+	const bind = typeof addr === 'string'? 'pipe ' + addr: 'port ' + addr.port
+	debug('Listening on ' + bind)
+})
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/public', express.static (__dirname + '/public'))
+app.use('/', express.static (__dirname + '/public'))
+app.use('/node_modules', express.static (__dirname + '/node_modules'))
