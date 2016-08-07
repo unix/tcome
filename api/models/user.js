aa8b7e296ebc4c8d6f52d6c5fe2c1ccab1af25c7
WittBulter <nanazuimeng123@gmail.com>
2016-08-07 22:34:15 +0800

    user Schema

--- api/models/user.js
+++ api/models/user.js
@@ -5,8 +5,17 @@
 const mongoose = require('mongoose')
 const conn = require("../mongo")
 
-console.log(conn);
+conn.model('Users', new mongoose.Schema({
+	username: String,           //用户名
+	passwordHash: String,
+	passwordSalt: String,
+	created_at: Date,           //创建时间
+	summary: String,            //简介
+	email: String,
+	phone: Number,
+	userType: Number,           //用户类型,普通用户为0
+}))
 
-module.exports = {
+module.exports = conn.model('Users')
 
-}
+
