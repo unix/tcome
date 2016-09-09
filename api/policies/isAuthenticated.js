0ffa6ca5ec1f2bafb48991af2de57e89e35a8d01
WittBulter <nanazuimeng123@gmail.com>
2016-09-09 17:55:18 +0800

    权限判断

---
+++ api/policies/isAuthenticated.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param req
+ * @param res
+ * @param next {}
+ * @returns {}
+ * @description :: 接口登录认证逻辑
+ */
+
+module.exports = (req, res, next) =>{
+	const clientToken = req.headers.authorization
+	if (!clientToken){
+		res.status(401)
+		return res.json({message: '未登录或token已过期'})
+	}
+
+	SessionService.findSession(clientToken, (err, sessionData) =>{
+		if (!sessionData){
+			res.status(401)
+			return res.json({message: '未登录或token已过期'})
+		}
+		/**
+		 *
+		 * @description 通过验证, 将email保存在header中 后续验证不再查询email
+		 */
+		req.headers.email = sessionData.email
+		req.headers.userID = sessionData.userID
+		return next()
+	})
+}
---
+++ api/policies/notCreated.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @description :: 用户创建权限验证 检查数据库中是否已经有用户
+ */
+module.exports = function (req, res, next){
+	UserService.findUser(req.allParams().email, (err, users) =>{
+		if (users.length > 0){
+			res.status(401)
+			return res.json({message: '该邮箱已被注册'})
+		}
+		return next()
+	})
+}
