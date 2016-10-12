6a12635b97756dc6f4588e7f9bb8d864034f0b05
WittBulter <nanazuimeng123@gmail.com>
2016-10-12 17:41:10 +0800

    添加Session模型

---
+++ api/models/Session.js
@@ -0,0 +1,19 @@
+/**
+ * Created by WittBulter on 2016/10/12.
+ * @description :: 储存用户会话资料
+ */
+
+module.exports = {
+	attributes: {
+		email: {
+			type: 'email',
+			required: true
+		},
+		clientToken: {
+			type: 'string',
+		},
+		userID: {
+			type: 'string'
+		}
+	}
+}
