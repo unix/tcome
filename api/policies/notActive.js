c6deca29393a91e6478897fb8934cb47d83850b0
WittBulter <nanazuimeng123@gmail.com>
2016-12-11 20:23:05 +0800

    完善用户激活权限逻辑

--- api/policies/notActive.js
+++ api/policies/notActive.js
@@ -9,10 +9,12 @@ module.exports = function (req, res, next){
 	const {id} = req.params
 	if (!id) return res.badRequest({message: '需要一个用户id'})
 	UserService.findUserForId(id, (err, user) =>{
+		if (err) return res.serverError()
+
 		if (!user|| !user.id) return res.forbidden({message: '该用户不存在'})
-		if (user.userType != 'notActive'){
-			return res.forbidden({message: '该用户无需再次验证'})
-		}
+		if (user.userType != 'notActive') return res.forbidden({message: '该用户无需再次验证'})
+		if (!user.activeTarget) return res.forbidden({message: '请重新发送验证邮件'})
+
 		return next()
 	})
 }
