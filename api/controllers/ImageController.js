96e028b5b96aa477b70cfba0b6afb32465a6b94d
WittBulter <nanazuimeng123@gmail.com>
2017-01-31 19:43:39 +0800

    增加上传图片

--- api/controllers/ImageController.js
+++ api/controllers/ImageController.js
@@ -1,6 +1,13 @@
 /**
  * Created by WittBulter on 2017/1/31.
  */
+const request = require('request')
+const config = require('../../ecosystem.config')
+const QiNiuCloud = require('qiniu')
+
+// 个人qiniu密钥配置
+QiNiuCloud.conf.ACCESS_KEY = config.qiniu.ACCESS_KEY
+QiNiuCloud.conf.SECRET_KEY = config.qiniu.SECRET_KEY
 
 module.exports = {
 
@@ -9,14 +16,34 @@ module.exports = {
 	 * @api {POST} http://wittsay.cc/api/image [create]
 	 * @apiGroup Image
 	 * @apiDescription 上传一张图片 需要登录
-	 * @apiParam (body) {string} image 图片
+	 * @apiParam (body) {string} image base64编码图片 转码后长度需小于2400000
+	 * @apiParam (body) {string} size 图片原大小
 	 * @apiUse CODE_200
 	 * @apiUse CODE_500
 	 */
 	upload: (req, res) =>{
-		// const {image} = req.allParams()
-		// console.log(image);
+		let {image, size} = req.allParams()
+		if (!image|| !size) return res.badRequest({message: '参数错误'})
+		if (image.length > 2400000){
+			return res.badRequest({message: '图片过大，请压缩后再尝试'})
+		}
+		const token = new QiNiuCloud.rs.PutPolicy('static').token()
+		if (image.includes('base64,')) image = image.split('base64,')[1]
 
-		res.ok({message: 'ok'})
+		request({
+			url: `http://up.qiniu.com/putb64/${size}`,
+			port: 8080,
+			method: 'POST',
+			body: image,
+			headers: {
+				'User-Agent': 'nodejs',
+				'Content-Type': 'application/octet-stream',
+				'Authorization': `UpToken ${token}`,
+			}
+		}, (err, response, body) =>{
+			if (err) return res.serverError(err)
+
+			res.ok(body)
+		})
 	}
 }
