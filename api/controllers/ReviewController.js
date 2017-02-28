a96bd908c25e9b96f47c9cd90b8ec483e19d7d82
WittBulter <nanazuimeng123@gmail.com>
2017-03-01 01:28:13 +0800

    修复审核文章的状态错误更新问题

--- api/controllers/ReviewController.js
+++ api/controllers/ReviewController.js
@@ -53,7 +53,7 @@ module.exports = {
 	update: async (req, res) =>{
 		const {id, status} = req.params
 		if (!id || !status) return res.badRequest({message: '参数错误'})
-		if (status != 'isReview'|| status != 'isActive'|| status != 'isDestroy'){
+		if (status != 'isReview'&& status != 'isActive'&& status != 'isDestroy'){
 			return res.badRequest({message: '状态错误'})
 		}
 		try {
